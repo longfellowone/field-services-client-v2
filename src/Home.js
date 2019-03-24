ea44d77772c380511e72cdf07baa96635f63266b
Matt Wright <mattwright3195@gmail.com>
2019-03-24 10:57:17 -0700

    init

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+  return (
+    <section id="container">
+      <header>Header</header>
+      <nav>Navigation</nav>
+      <main>Main area</main>
+      <footer>Footer</footer>
+    </section>
+  );
+};
+
+export default Home;
