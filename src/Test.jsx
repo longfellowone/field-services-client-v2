abb4b8156ad30d042da2d08d0fc10080cbdde490
Matt Wright <mattwright3195@gmail.com>
2019-03-27 16:36:00 -0700

    new test search complete

--- src/Test.jsx
+++ src/Test.jsx
@@ -7,6 +7,7 @@ export const Test = () => {
     <>
       <div className="container">
         <div className="box">
+          {/* style={{ paddingLeft: "0.5rem" }} */}
           <Search />
         </div>
         <Items />
@@ -17,6 +18,7 @@ export const Test = () => {
 
 const Search = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [input, setInput] = useState('');
   const inputRef = useRef();
 
   const handleFocus = () => {
@@ -25,8 +27,17 @@ const Search = () => {
   };
 
   const handleSubmit = () => {
+    closeSearch();
+  };
+
+  const closeSearch = () => {
+    setInput('');
     setMenuOpen(false);
     document.body.classList.remove('noscrollbody');
+  };
+
+  const handleChange = e => {
+    setInput(e.target.value);
   };
 
   useEffect(() => {
@@ -34,33 +45,46 @@ const Search = () => {
   }, []);
 
   return (
-    <>
-      <div className={menuOpen ? 'overlay' : 'flex'} onTouchStart={() => inputRef.current.blur()}>
-        <div className={menuOpen ? 'container' : 'flex'}>
-          <div className={menuOpen ? 'search' : 'flex'}>
-            <div className="back" style={{ display: !menuOpen ? 'none' : 'none' }}>
-              B
-            </div>
-            <input
-              ref={inputRef}
-              className="searchinput"
-              onFocus={handleFocus}
-              onTouchStart={e => e.stopPropagation()}
-              maxLength="50"
-              placeholder="Search for an item..."
-              tabIndex="0"
-              type="search"
+    <div className={menuOpen ? 'overlay' : 'flex'} onTouchStart={() => inputRef.current.blur()}>
+      <div className={menuOpen ? 'container' : 'flex'}>
+        <form action="." className={menuOpen ? 'search' : 'flex'}>
+          {/* <div
+            className="back"
+            style={{ display: menuOpen ? "" : "none" }}
+            onTouchStart={e => e.stopPropagation()}
+            onMouseDown={handleClose}
+          >
+            <img
+              src="https://img.icons8.com/ios/25/000000/left.png"
+              alt="back"
             />
-            <div className="close" style={{ display: !menuOpen ? 'none' : 'none' }}>
-              X
-            </div>
+          </div> */}
+          <input
+            ref={inputRef}
+            value={input}
+            className="searchinput"
+            onTouchStart={e => e.stopPropagation()}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            maxLength="50"
+            placeholder="Search for an item..."
+            tabIndex="0"
+            type="search"
+          />
+          <div
+            className="delete"
+            style={{ display: menuOpen ? '' : 'none' }}
+            onTouchStart={e => e.stopPropagation()}
+            onMouseDown={closeSearch}
+          >
+            <img src="https://img.icons8.com/ios/15/000000/delete-sign.png" alt="delete" />
           </div>
-          <div className="results" style={{ display: !menuOpen ? 'none' : '' }}>
-            <Results handleSubmit={handleSubmit} />
-          </div>
+        </form>
+        <div className="results" style={{ display: !menuOpen ? 'none' : '' }}>
+          <Results handleSubmit={handleSubmit} />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
@@ -69,6 +93,12 @@ const Results = ({ handleSubmit }) => {
   for (let i = 0; i < 10; i++) {
     results.push(
       <div key={Math.random()} className="result" onMouseDown={handleSubmit}>
+        {/* <div className="add">
+          <img
+            src="https://img.icons8.com/color/20/000000/plus.png"
+            alt="add"
+          />
+        </div> */}
         <div className="name">Result</div>
         <div className="uom">ft</div>
       </div>,
@@ -88,7 +118,15 @@ const Items = () => {
       </div>,
     );
   }
-
+  for (let i = 0; i < 1; i++) {
+    items.push(
+      <div key={Math.random()} className="box">
+        <div className="name">Item (New)</div>
+        <QuantityInput quantity="100" />
+        <div className="uom">ft</div>
+      </div>,
+    );
+  }
   return <>{items}</>;
 };
 
@@ -101,25 +139,18 @@ const QuantityInput = ({ quantity, productID, orderID }) => {
   const handleBlur = (e, func) => {
     setEditable(false);
 
-    console.log(isNaN(e.target.value));
-
     if (e.target.value === '' || isNaN(e.target.value)) {
       setInput(0);
       return;
       // return func({ variables: { input: 0 } });
     }
 
-    setInput(parseInt(e.target.value));
+    setInput(parseInt(e.target.value, 10));
     // func({ variables: { input: e.target.value } });
   };
 
   const handleChange = e => {
     setInput(e.target.value);
-  };
-
-  const handleFocus = e => {
-    e.preventDefault();
-    e.target.select();
   };
 
   return editable ? (
@@ -128,7 +159,7 @@ const QuantityInput = ({ quantity, productID, orderID }) => {
         ref={focusInput}
         value={input}
         onChange={handleChange}
-        onFocus={handleFocus}
+        onFocus={e => e.target.select()}
         onBlur={e => handleBlur(e)}
         pattern="[0-9]*"
         type="tel"
